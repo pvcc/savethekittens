4c515e0dd1aef549b1344b98d439bd1bf9cefeb8
George Murphy <gmurphy@pennwoodmanion.com>
2016-12-09 18:18:21 -0500

    Initial Commit

---
+++ savethekittens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  savethekittens
+//
+//  Created by George Murphy on 12/9/16.
+//  Copyright © 2016 George Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
